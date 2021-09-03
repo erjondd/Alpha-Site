d3841c567380ce2c59b693f427934fc049002481
julif3r <hajrullah.islami@gmail.com>
2021-09-03 12:55:51 +0200

    [fix] - Hover style

--- wp-content/themes/VD-theme/header.php
+++ wp-content/themes/VD-theme/header.php
@@ -36,13 +36,22 @@ if (!$has_page_banner) {
 
     <div class="menu-wrapper">
         <div id="menu-open-trigger" class="menu-icon">
-            <img src="<?= get_template_directory_uri() ?>/images/menu-icon.svg" alt="Menu Open" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="66.54" height="66.679" viewBox="0 0 66.54 66.679">
+                <g id="Group_4" data-name="Group 4" transform="translate(160.764 -1723.904) rotate(90)">
+                    <path id="Path_10" data-name="Path 10" d="M423.588,218.1l11.856,33.218,6.17,17.3,5.72,16.026h12.341l-11.89-33.322-6.171-17.3L435.933,218.1Z" transform="translate(1300.316 -123.873)" fill="#fff" />
+                    <path id="Path_11" data-name="Path 11" d="M423.8,218.1l11.71,33.218,6.095,17.3,5.65,16.026h12.189L447.7,251.316l-6.095-17.3L435.994,218.1Z" transform="translate(1331.138 -123.873)" fill="#fff" />
+                </g>
+            </svg>
+
         </div>
 
         <div class="menu-content menu-black">
 
             <div class="menu-icon-x">
-                <img src="<?= get_template_directory_uri() ?>/images/menu-icon-x.svg" alt="Menu Close" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="111.723" height="111.723" viewBox="0 0 111.723 111.723">
+                    <path id="Union_1" data-name="Union 1" d="M55.861,74.246,18.384,111.723,0,93.338,37.476,55.862,0,18.385,18.385,0,55.862,37.476,93.338,0l18.384,18.385L74.246,55.861l37.477,37.477L93.338,111.723Z" fill="#fff" />
+                </svg>
+
             </div>
             <div class="menu-links">
                 <div class="menu-batiment menu-open-black"><img src="<?= get_template_directory_uri() ?>/images/batiment-homepage.svg" alt=""></div>
@@ -60,7 +69,9 @@ if (!$has_page_banner) {
         <div class="menu-content menu-white">
 
             <div class="menu-icon-x">
-                <img src="<?= get_template_directory_uri() ?>/images/menu-icon-x.svg" alt="Menu Close" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="111.723" height="111.723" viewBox="0 0 111.723 111.723">
+                    <path id="Union_1" data-name="Union 1" d="M55.861,74.246,18.384,111.723,0,93.338,37.476,55.862,0,18.385,18.385,0,55.862,37.476,93.338,0l18.384,18.385L74.246,55.861l37.477,37.477L93.338,111.723Z" fill="#fff" />
+                </svg>
             </div>
             <div class="menu-links">
                 <div class="menu-batiment menu-open-black"><img src="<?= get_template_directory_uri() ?>/images/batiment-homepage.svg" alt=""></div>
--- wp-content/themes/VD-theme/template-valeurs-edification.php
+++ wp-content/themes/VD-theme/template-valeurs-edification.php
@@ -9,7 +9,7 @@ Template Name: Valeurs Edification
 ?>
 
 
-<?php  
+<?php
 
 $valeur_left_bold_text = get_field("valeur_left_bold_text");
 $valeurs_left_normal_text = get_field("valeurs_left_normal_text");
@@ -43,7 +43,7 @@ $valeurs_submain_tittle_3 = get_field("valeurs_submain_tittle_3");
       <div class="page-content-info-main">
         <img src="<?= get_template_directory_uri() ?>/images/no_vals.svg">
         <p class="info-main-bold">
-        <?= $valeur_left_bold_text ?>
+          <?= $valeur_left_bold_text ?>
         </p>
         <p class="info-main-normal">
           <?= $valeurs_left_normal_text ?>
@@ -58,7 +58,7 @@ $valeurs_submain_tittle_3 = get_field("valeurs_submain_tittle_3");
 
         <div class="info-box box2">
           <div class="text-box">
-          <?= $valeurs_box_2 ?>
+            <?= $valeurs_box_2 ?>
           </div>
         </div>
 
@@ -73,7 +73,13 @@ $valeurs_submain_tittle_3 = get_field("valeurs_submain_tittle_3");
     </div>
     <div class="page-content-slider">
       <div class="svg-up valuers-arrow-up">
-        <img src="<?= get_template_directory_uri() ?>/images/arrow-up.svg" alt="arrow up" />
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="62.43" height="22.459" viewBox="0 0 62.43 22.459">
+          <g id="Group_8" data-name="Group 8" transform="translate(62.43 22.459) rotate(180)">
+            <path id="Path_31" data-name="Path 31" d="M22.423,31.166H10.987L5.823,16.515l-.1-.288-.036-.1L0,0H11.44l5.265,14.943Z" transform="translate(31.263 22.459) rotate(-90)" fill="#fff" />
+            <path id="Path_32" data-name="Path 32" d="M22.459,0,16.741,16.227v0L11.472,31.166l-.032.1H0l.036-.1,5.686-16.13.032-.1L11.019,0Z" transform="translate(0 22.459) rotate(-90)" fill="#fff" />
+          </g>
+        </svg>
       </div>
       <div class="content-slider-info">
         <span class="vision"><?= $valeurs_slider_1 ?></span>
@@ -81,7 +87,10 @@ $valeurs_submain_tittle_3 = get_field("valeurs_submain_tittle_3");
         <span class="mission"><?= $valeurs_slider_3 ?></span>
       </div>
       <div class="svg-down valuers-arrow-down">
-        <img src="<?= get_template_directory_uri() ?>/images/arrow-down.svg" alt="arrow up" />
+        <svg id="Group_9" data-name="Group 9" xmlns="http://www.w3.org/2000/svg" width="62.43" height="22.459" viewBox="0 0 62.43 22.459">
+          <path id="Path_31" data-name="Path 31" d="M22.423,31.166H10.987L5.823,16.515l-.1-.288-.036-.1L0,0H11.44l5.265,14.943Z" transform="translate(31.263 22.459) rotate(-90)" fill="#fff" />
+          <path id="Path_32" data-name="Path 32" d="M22.459,0,16.741,16.227v0L11.472,31.166l-.032.1H0l.036-.1,5.686-16.13.032-.1L11.019,0Z" transform="translate(0 22.459) rotate(-90)" fill="#fff" />
+        </svg>
       </div>
     </div>
   </div>
